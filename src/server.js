cd60263ffe38bf42f9b19ebb297a5f56a8b3da52
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2023-03-30 02:45:33 -0300

    Habilitando o cors

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json()) //ativa o body
+app.use(cors({
+    origin: '*'
+}));
 app.use(routes);
 
 
